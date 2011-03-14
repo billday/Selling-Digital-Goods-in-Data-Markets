1485534644296c139a45f15eadb8d601be40bf6f
Bill Day <bill@billday.com>
2011-03-14 14:49:25 -0500

    Added 'use' portion of YQL statement so that Python-YQL knows to use the infochimps.influence.xml data table

--- datamarkets.infochimps.yql.py
+++ datamarkets.infochimps.yql.py
@@ -3,8 +3,8 @@
 #
 # Copyright (c) 2011, Bill Day; for more information see http://billday.com
 
-# Import the modules supporting Yahoo! Query Language operations.  For
-# more information on the Python-YQL module, please refer to:
+# Import the package Python-YQL which supports Yahoo! Query Language
+# operations.  For more information, please refer to:
 # http://python-yql.org/
 #
 import yql
@@ -13,8 +13,8 @@ import yql
 #
 y = yql.Public()
 
-# Specify Twitter screen name for which we wish to retrieve influence metrics
-# and the Infochimps apikey to use.
+# Specify Twitter screen name for which we wish to retrieve
+# influence metrics and the Infochimps apikey to use.
 #
 screenname = "billday"
 apikey = "api_test-W1cipwpcdu9Cbd9pmm8D4Cjc469"
@@ -23,25 +23,21 @@ apikey = "api_test-W1cipwpcdu9Cbd9pmm8D4Cjc469"
 # yql.YQLObj containing rows where each row contains a 
 # dictionary whose value contains key:value pairs.
 #
-# For more information see:
-# http://python-yql.org/code.html
+# Note that we need to include the "use" portion of the
+# YQL statement so that the Python YQL package knows it
+# needs to reference that data table.  For more see:
+# https://bugs.launchpad.net/python-yql/+bug/664360
 #
-influencequery = "select * from infochimps.influence where screen_name='" + screenname + "' and apikey='" + apikey + "';"
+influencequery = "use 'http://www.datatables.org/infochimps/infochimps.influence.xml'; select * from infochimps.influence where screen_name='" + screenname + "' and apikey='" + apikey + "'"
+metrics = y.execute(influencequery)
 
+# Print out what we fetched.
+#
 print "-----"
 print "Retrieving influence metrics for Twitter screen name: ", screenname
 print "-----"
-
-# For some reason, YQL is currently returning:
-# "No definition found for Table infochimps.influence"
-# when we execute the YQL call below even though the exact 
-# same YQL statement executes correctly in the YQL console
-# via this call:
-# http://yhoo.it/gYZ17r
-#
-# I am investigating and will update this example program as needed.
-# 
-metrics = y.execute(influencequery)
+for row in metrics.rows:
+    print row
 
 # Now that we have the data in our Python program, we can add code to
 # do whatever else we want with the metrics.  Enjoy!
